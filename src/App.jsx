adeb50a33364e6ec1c1128bd026fb66b9c277c3d
Barry-byte-cyber <metisman@telus.net>
2025-07-10 09:10:41 -0600

    Final deploy-ready push for Lady Pant Store

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Lady Pant Store Booking</h1>
+      <p>Welcome! This is your deployed Vite + React app.</p>
+    </div>
+  );
+}
+
+export default App;
